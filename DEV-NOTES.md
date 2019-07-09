841e78e3424d0fd07f4cba857be2e51b19af4651
onoroff <adam.grunke@gmail.com>
2019-07-09 15:38:49 -0700

    create new item in db and show when db is empty

--- DEV-NOTES.md
+++ DEV-NOTES.md
@@ -11,3 +11,32 @@ migrate model --- complete
 sequelize db:migrate
 
 check for table --- complete
+
+created index.ejs
+
+root route --- complete
+linked css stylesheet --- complete
+
+no widgets exist displayed when db is empty.
+
+<%=widget.description%>
+<%=widget.quantity%>
+
+
+    <% widgets.forEach(widget){ %>
+        <div class="section">
+        <h2>Descript</h2>
+        <p>Quantity: </p>
+        </div>
+    <% }; %>
+<% }; %>
+
+
+<%if (widgets.length === 0) { %>
+    <h3> No widgets exist</h3>
+<% } else { %>
+<% widgets.forEach(widget){ %>
+    <p> <%=%> </p>
+
+<% } %>
+<% };%>
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const db = require('./models');
 
 const app = express();
 
@@ -10,7 +11,27 @@ app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
+app.get('/', function(req, res) {
+    db.widget.findAll().then(function(widgets){
+        res.render('index',{widgets})
+    })
+})
 
+app.post('/', function(req, res) {
+    db.widget.create({
+        description: req.body.description,
+        quantity: req.body.quantity
+    })
+        res.redirect('/')
+    })
+
+    app.delete('/:id', function(req, res) {
+        db.widget.destroy({
+            where: {id: parseInt(req.body.id)}
+        }).then(function() {
+            res.redirect('/');
+            });
+        });
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
